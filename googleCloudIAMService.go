68b3dbdc3ef78a58ee8bef66e5699a29684aff10
Jorrit Salverda <jsalverda@travix.com>
2019-01-30 14:58:10 +0100

    fix return with shadowed err variable

--- googleCloudIAMService.go
+++ googleCloudIAMService.go
@@ -111,7 +111,7 @@ func (googleCloudIAMService *GoogleCloudIAMService) GetServiceAccountByDisplayNa
 		}
 		resp, err := listCall.Context(context.Background()).Do()
 		if err != nil {
-			return
+			return "", err
 		}
 
 		// filter on display names
